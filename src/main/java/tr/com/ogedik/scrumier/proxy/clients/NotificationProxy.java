0ee5e0b3c0b0c48ada8e6f002b5fab15ec0a14aa
Mehmet Enes Erciyes <merciyes18@ku.edu.tr>
2020-11-23 18:50:33 +0300

    First email sent successfully

--- src/main/java/tr/com/ogedik/scrumier/proxy/clients/NotificationProxy.java
+++ src/main/java/tr/com/ogedik/scrumier/proxy/clients/NotificationProxy.java
@@ -1,8 +1,14 @@
 package tr.com.ogedik.scrumier.proxy.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import tr.com.ogedik.commons.constants.Services;
+import tr.com.ogedik.commons.rest.request.model.EmailRequest;
 
 @FeignClient(name = Services.NOTIFICATION)
 public interface NotificationProxy {
+
+    @PostMapping(Services.Path.MAIL)
+    void sendMail(@RequestBody EmailRequest request);
 }
